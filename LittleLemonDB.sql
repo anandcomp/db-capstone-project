fe47585bf7841e2aa604b46fb73592bb602ed1fd
Anand Ganesan <anand@allied.com.sg>
2024-12-18 11:56:02 +0530

    add stored procedure

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -23,11 +23,11 @@ DROP TABLE IF EXISTS `booking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `booking` (
-  `BookingID` int NOT NULL AUTO_INCREMENT,
-  `Date` date NOT NULL,
+  `BookingID` int NOT NULL,
+  `Date` datetime NOT NULL,
   `TableNumber` int NOT NULL,
   PRIMARY KEY (`BookingID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,12 +47,12 @@ DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `customer` (
-  `CustomerID` int NOT NULL AUTO_INCREMENT,
-  `CustomerName` varchar(45) NOT NULL,
+  `CustomerID` int NOT NULL,
+  `FullName` varchar(255) NOT NULL,
   `ContactNumber` varchar(45) NOT NULL,
-  `Email` varchar(45) NOT NULL,
+  `Email` varchar(255) NOT NULL,
   PRIMARY KEY (`CustomerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,10 +72,10 @@ DROP TABLE IF EXISTS `deliverystatus`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `deliverystatus` (
-  `DeliveryStatusID` int NOT NULL AUTO_INCREMENT,
+  `DeliveryID` int NOT NULL,
   `DeliveryDate` datetime NOT NULL,
-  PRIMARY KEY (`DeliveryStatusID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`DeliveryID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,11 +95,11 @@ DROP TABLE IF EXISTS `menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `menu` (
-  `MenuId` int NOT NULL AUTO_INCREMENT,
-  `MenuName` varchar(45) NOT NULL,
-  `MenuDescription` varchar(45) NOT NULL,
-  PRIMARY KEY (`MenuId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `MenuID` int NOT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`MenuID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -112,6 +112,72 @@ LOCK TABLES `menu` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `OrderID` int NOT NULL,
+  `Date` datetime NOT NULL,
+  `Quantity` int NOT NULL,
+  `TotalCost` decimal(10,0) DEFAULT NULL,
+  `Booking_BookingID` int NOT NULL,
+  `Customer_CustomerID` int NOT NULL,
+  `Menu_MenuID` int NOT NULL,
+  `DeliveryStatus_DeliveryID` int NOT NULL,
+  PRIMARY KEY (`OrderID`,`Customer_CustomerID`,`Menu_MenuID`,`DeliveryStatus_DeliveryID`),
+  KEY `fk_Orders_Booking_idx` (`Booking_BookingID`),
+  KEY `fk_Orders_Customer1_idx` (`Customer_CustomerID`),
+  KEY `fk_Orders_Menu1_idx` (`Menu_MenuID`),
+  KEY `fk_Orders_DeliveryStatus1_idx` (`DeliveryStatus_DeliveryID`),
+  CONSTRAINT `fk_Orders_Booking` FOREIGN KEY (`Booking_BookingID`) REFERENCES `booking` (`BookingID`),
+  CONSTRAINT `fk_Orders_Customer1` FOREIGN KEY (`Customer_CustomerID`) REFERENCES `customer` (`CustomerID`),
+  CONSTRAINT `fk_Orders_DeliveryStatus1` FOREIGN KEY (`DeliveryStatus_DeliveryID`) REFERENCES `deliverystatus` (`DeliveryID`),
+  CONSTRAINT `fk_Orders_Menu1` FOREIGN KEY (`Menu_MenuID`) REFERENCES `menu` (`MenuID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `ordersview`
+--
+
+DROP TABLE IF EXISTS `ordersview`;
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `ordersview` AS SELECT 
+ 1 AS `OrderID`,
+ 1 AS `Quantity`,
+ 1 AS `TotalCost`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `ordersview2`
+--
+
+DROP TABLE IF EXISTS `ordersview2`;
+/*!50001 DROP VIEW IF EXISTS `ordersview2`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `ordersview2` AS SELECT 
+ 1 AS `CustomerID`,
+ 1 AS `FullName`,
+ 1 AS `OrderID`,
+ 1 AS `TotalCost`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `staff`
 --
 
@@ -119,17 +185,17 @@ DROP TABLE IF EXISTS `staff`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `staff` (
-  `StaffID` int NOT NULL AUTO_INCREMENT,
-  `StaffName` varchar(45) NOT NULL,
+  `StaffID` int NOT NULL,
+  `FullName` varchar(255) NOT NULL,
   `ContactNumber` varchar(45) NOT NULL,
-  `Email` varchar(45) NOT NULL,
+  `Email` varchar(255) NOT NULL,
   `Role` varchar(45) NOT NULL,
-  `Salary` decimal(10,2) NOT NULL,
-  `CustomerID` int NOT NULL,
+  `Salary` decimal(10,0) NOT NULL,
+  `Customer_CustomerID` int NOT NULL,
   PRIMARY KEY (`StaffID`),
-  KEY `FK_Customer_id_idx` (`CustomerID`),
-  CONSTRAINT `FK_Customer_id` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_Staff_Customer1_idx` (`Customer_CustomerID`),
+  CONSTRAINT `fk_Staff_Customer1` FOREIGN KEY (`Customer_CustomerID`) REFERENCES `customer` (`CustomerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,6 +206,42 @@ LOCK TABLES `staff` WRITE;
 /*!40000 ALTER TABLE `staff` DISABLE KEYS */;
 /*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `ordersview`
+--
+
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`developer`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `ordersview` AS select `orders`.`OrderID` AS `OrderID`,`orders`.`Quantity` AS `Quantity`,`orders`.`TotalCost` AS `TotalCost` from `orders` where (`orders`.`Quantity` > 2) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `ordersview2`
+--
+
+/*!50001 DROP VIEW IF EXISTS `ordersview2`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`developer`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `ordersview2` AS select `customer`.`CustomerID` AS `CustomerID`,`customer`.`FullName` AS `FullName`,`orders`.`OrderID` AS `OrderID`,`orders`.`TotalCost` AS `TotalCost` from (`customer` join `orders`) where (`orders`.`TotalCost` > 150) order by `orders`.`TotalCost` desc */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -150,4 +252,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-14 14:37:20
+-- Dump completed on 2024-12-18 11:54:19
